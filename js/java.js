86021a835e54d4f52fecaa55c505c53d2f5e90b8
ViniciusFelce <viramos017@gamil.com>
2022-02-15 18:18:17 -0300

    aula 15/02

---
+++ js/java.js
@@ -0,0 +1,20 @@
+/*
+const nome = "jose";
+const sobre = "silva";
+const idade = 27;
+const peso = 83.5; 
+const altura = 1.70;
+const plano = "ouro";
+
+const IMC = peso / (altura*altura);
+
+console.log("Nome " + nome  + ": " + "Sobre " + sobre  + " " +  "Idade " + idade + " " +  "Peso " + peso + " "+  "Altura " + altura + " "  +  "Plano " + plano + " " + IMC);
+  */        
+         
+         
+         function imc (nome,sobrenome,idade,peso,altura,plano){
+             
+            console.log("Nome " + nome  + ": " + "Sobre " + sobre  + " " +  "Idade " + idade + " " +  "Peso " + peso + " "+  "Altura " + altura + " "  +  "Plano " + plano + " " + imc);
+
+         }
+        imc ("jose","da silva",27,83.5,1.70,"ouro")
