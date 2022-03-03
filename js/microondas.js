b952abe7f49890c6ab1afe2dca0517e99751c386
ViniciusFelce <viramos017@gamil.com>
2022-03-03 20:20:50 -0300

    micro

---
+++ js/microondas.js
@@ -0,0 +1,102 @@
+//GRUPO - 8 //Vinicius R. Felce
+            //Vinicius Sesma
+            //Vinícius Marques
+            //Felipe Godoy
+
+let PesoPrato = 1.34;
+
+// 1 - Pipoca – 10 segundos (padrão); Prato pronto, Boa apetite!!!
+let Pipoca = 10;
+
+if (PesoPrato < 1.34){
+    console.log("Pipoca – 10 segundos (padrão)" +"\n" + "Tempo Escolhido: 10 segundos" + "\n", "Prato inexistente" +"\n")
+}
+ else if (Pipoca < 10 ) {
+    console.log ("Pipoca – 10 segundos (padrão)" +"\n" + "Tempo Escolhido: 10 segundos" + "\n", "Tempo insuficiente" +"\n")
+}
+ else if (Pipoca >= 20 && Pipoca <30 ){
+    console.log("Pipoca – 10 segundos (padrão)" +"\n" + "Tempo Escolhido: 10 segundos" + "\n", "Comida queimou." +"\n")
+}
+else if (Pipoca >= 30 ){
+    console.log("Pipoca – 10 segundos (padrão)" +"\n" + "Tempo Escolhido: 10 segundos" + "\n", "Kabumm" +"\n")
+}
+else {
+    console.log("Pipoca – 10 segundos (padrão)" +"\n" + "Tempo Escolhido: 10 segundos" + "\n", "Prato pronto, Boa apetite!!!" +"\n")
+}
+
+// 2 - Macarrão – 8 segundos (padrão); Kabumm
+let Macarrao = 25;
+
+if (PesoPrato < 1.34){
+    console.log("Macarrao – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Prato inexistente" +"\n")
+}
+ else if (Macarrao < 8 ) {
+    console.log ("Macarrao – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Tempo insuficiente" +"\n")
+}
+ else if (Macarrao >= 16 && Macarrao <24 ){
+    console.log("Macarrao – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Comida queimou." +"\n")
+}
+else if (Macarrao >= 24 ){
+    console.log("Macarrao – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Kabumm" +"\n")
+}
+else {
+    console.log("Macarrao – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Prato pronto, Boa apetite!!!" +"\n")
+}
+
+//3 - Carne – 15 segundos (padrão); Comida queimou.
+let Carne = 30;
+
+if (PesoPrato < 1.34){
+    console.log("Carne – 15 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Prato inexistente" +"\n")
+}
+ else if (Carne < 15 ) {
+    console.log ("Carne – 15 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Tempo insuficiente" +"\n")
+}
+ else if (Carne >= 30 && Carne <45 ){
+    console.log("Carne – 15 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Comida queimou." +"\n")
+}
+else if (Carne >= 45 ){
+    console.log("Carne – 15 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Kabumm" +"\n")
+}
+else {
+    console.log("Carne – 15 segundos (padrão)" +"\n" + "Tempo Escolhido: 25 segundos" + "\n", "Prato pronto, Boa apetite!!!" +"\n")
+}
+
+// 4 - Feijão – 12 segundos (padrão); Tempo insuficiente
+let Feijao = 9;
+
+if (PesoPrato < 1.34){
+    console.log("Feijao – 12 segundos (padrão)" +"\n" + "Tempo Escolhido: 9 segundos" + "\n", "Prato inexistente" +"\n")
+}
+ else if (Feijao < 12 ) {
+    console.log ("Feijao – 12 segundos (padrão)" +"\n" + "Tempo Escolhido: 9 segundos" + "\n", "Tempo insuficiente" +"\n")
+}
+ else if (Feijao >= 24 && Feijao <36 ){
+    console.log("Feijao – 12 segundos (padrão)" +"\n" + "Tempo Escolhido: 9 segundos" + "\n", "Comida queimou." +"\n")
+}
+else if (Feijao >= 36 ){
+    console.log("Feijao – 12 segundos (padrão)" +"\n" + "Tempo Escolhido: 9 segundos" + "\n", "Kabumm" +"\n")
+}
+else {
+    console.log("Feijao – 12 segundos (padrão)" +"\n" + "Tempo Escolhido: 9 segundos" + "\n", "Prato pronto, Boa apetite!!!" +"\n")
+}
+
+// 5 - Brigadeiro – 8 segundos (padrão); Prato inexistente
+let Brigadeiro = 8;
+let PesoPrato2 = 0;
+
+if (PesoPrato2 < 1.34){
+    console.log("Brigadeiro – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 8 segundos" + "\n", "Prato inexistente" +"\n")
+}
+ else if (Brigadeiro < 8 ) {
+    console.log ("Brigadeiro – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 8 segundos" + "\n", "Tempo insuficiente" +"\n")
+}
+ else if (Brigadeiro >= 16 && Brigadeiro <24 ){
+    console.log("Brigadeiro – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 8 segundos" + "\n", "Comida queimou." +"\n")
+}
+else if (Brigadeiro >= 24 ){
+    console.log("Brigadeiro – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 8 segundos" + "\n", "Kabumm" +"\n")
+}
+else {
+    console.log("Brigadeiro – 8 segundos (padrão)" +"\n" + "Tempo Escolhido: 8 segundos" + "\n", "Prato pronto, Boa apetite!!!" +"\n")
+}
